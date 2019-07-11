866830eed411e85d410e3668d07866b391df00bb
dariusASC6 <dfree82803@gmail.com>
2019-07-11 11:57:05 -0400

    first draft

---
+++ randomness.js
@@ -0,0 +1,22 @@
+function randomNumber(){
+    const number = Math.random();
+    console.log(number);
+    const times20 = number * 20;
+    console.log(times20);
+    const final = Math.floor(times20);
+    console.log(final)
+ }
+ 
+ randomNumber();
+
+ //New Code
+
+function randNumber() {
+ const decimal = Math.random();
+ //console.log(decimal);
+ const times21 = decimal * 21
+ //console.log(times10);
+ const final = Math.floor(times21);
+ console.log(final);
+}
+randNumber();
